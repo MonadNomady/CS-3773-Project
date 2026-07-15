ee5b5a5120f370ee34fc0fd1a3f30545a352459d
madisonperez <madisonm.perez@gmail.com>
2026-07-14 20:57:43 -0500

    Springboot files init

---
+++ online-grocery-portal/src/main/java/com/grocery/online_grocery_portal/OnlineGroceryPortalApplication.java
@@ -0,0 +1,13 @@
+package com.grocery.online_grocery_portal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineGroceryPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineGroceryPortalApplication.class, args);
+	}
+
+}
---
+++ online-grocery-portal/src/test/java/com/grocery/online_grocery_portal/OnlineGroceryPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grocery.online_grocery_portal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineGroceryPortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
